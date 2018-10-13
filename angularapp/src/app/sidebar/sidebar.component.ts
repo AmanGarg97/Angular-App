11538bbc5ba8cd2a097fa49bc6a4d32295a1fcb8
AmanGarg <amangarg34.ag@gmail.com>
2018-10-13 13:39:01 +0530

    bug fux

--- angularapp/src/app/sidebar/sidebar.component.ts
+++ angularapp/src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+
 export class SidebarComponent implements OnInit {
 
   currentUrl: string;
